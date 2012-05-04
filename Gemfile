82b2182ecf22fff4afd6ce3fca8c1eead41aaceb
javierg <javierg@amcoonline.net>
2012-05-03 19:37:47 -0700

    initital repo --no index.html

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'sqlite3'
+gem 'json'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platform => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H8::Application.config.secret_token = 'c984489cffaa9b496f2f30187ab787db8daf8d2b688a7a3a83c021d0a3140b7ef9d67ff7739cd41dbe1271a6cf1a61654d6ae758b014126805d0e61cd203d249'
